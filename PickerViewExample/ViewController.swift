2be3f0fd0891a328a374c3390d611243efad230a
Pravin Kandala <z1761257@DoITs-Mac-mini-2.local>
2015-10-05 09:35:05 -0500

    multiple component for pickerview
    example -->  (origin) to (destination)

--- PickerViewExample/ViewController.swift
+++ PickerViewExample/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        origin = names[0][pickerView.selectedRowInComponent(0)]
+        destination = names[1][pickerView.selectedRowInComponent(1)]
+        updateUI()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,22 +27,40 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     @IBOutlet weak var pickerView: UIPickerView!
     @IBOutlet weak var outputLabel: UILabel!
     
-    let names = ["New York", "Mascow", "Houston", "Chicago", "Bangalore", "Los Angeles"]
+    var origin = ""
+    var destination = ""
+    
+    let names = [["New York", "Mascow", "Houston", "Chicago", "Bangalore", "Los Angeles"],["New York", "Detroit", "Houston", "Chicago", "San Francisco", "Los Angeles"]]
     
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-        return 1
-    }
-
-    func pickerView(_pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
         return names.count
     }
     
+    func updateUI(){
+        outputLabel.text = "Flight from \(origin) to \(destination)"
+    }
+    
+    
+    func pickerView(_pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+        return names[component].count
+    }
+    
+    
+    
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return names[row]
+        return names [component][row]
     
     }
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int){
-        outputLabel.text = names[row]
+        if(component==0)
+        {
+            origin = names[component][row]
+        } else {
+            
+         destination = names[component][row]
+        }
+     //  outputLabel.text = names [component][row]
+        updateUI()
         
     }
 }
